06da903ca229288d03416990991700bb51668a9a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-05 02:18:16 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ class Backer
     @backed_projects
   end
   
-  def back_project(project)
-    @backed_projects << 
+  def back_project(Project.new(title))
+    @backed_projects << Project.new(title)
   end
 end
