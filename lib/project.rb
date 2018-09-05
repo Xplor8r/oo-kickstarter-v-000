c725afe2524292f7c3ad5cfe90a5d106bc349413
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-05 01:37:55 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,8 @@
 def Project
-  attr_accessor :name, :backers
+  attr_accessor :title, :backers
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
   end
   
